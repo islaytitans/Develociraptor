2b9186cf61c68ce700963f5a54eab1d174a3036a
Jonathan Robbins <jonathan.m.robbins@gmail.com>
2018-12-05 09:09:45 +0000

    dev / added first angular component for project listing

---
+++ Develociraptor.App/ClientApp/src/app/project-listing/project-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-project-listing',
+  templateUrl: './project-listing.component.html'
+})
+export class ProjectListingComponent {
+
+}
